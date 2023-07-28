0b712a8fbbd75a2d8446d2867b05b9dd2265680c
Kiran Anand <liteninkiran@hotmail.com>
2023-07-28 15:55:17 +0100

    03 - Create MongoDB database

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const connectDB = require('./server/config/db');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Connect to Database  
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
