6ca2d2e8151178e32135b7398b97c790c9cfa588
Kiran Anand <liteninkiran@hotmail.com>
2023-07-28 16:18:47 +0100

    04 - Add, update & delete user: Add form, controller & routes

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -1,3 +1,6 @@
+const Customer = require('../models/Customer');
+const mongoose = require('mongoose');
+
 /**
  * GET /
  * Homepage
@@ -9,3 +12,38 @@ exports.homepage = async (req, res) => {
     }
     res.render('index', { locals });
 }
+
+/**
+ * GET /
+ * New Customer Form
+ */
+exports.addCustomer = async (req, res) => {
+    const locals = {
+        title: 'Add New Customer - NodeJs',
+        description: 'Free NodeJs User Management System',
+    };
+
+    res.render('customer/add', locals);
+};
+
+/**
+ * POST /
+ * Create New Customer
+ */
+exports.postCustomer = async (req, res) => {
+    const newCustomer = new Customer({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        details: req.body.details,
+        tel: req.body.tel,
+        email: req.body.email,
+    });
+
+    try {
+        await Customer.create(newCustomer);
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
+};
--- server/routes/customer.js
+++ server/routes/customer.js
@@ -6,5 +6,7 @@ const customerController = require('../controllers/customerController');
  *  Customer Routes 
  */
 router.get('/', customerController.homepage);
+router.get('/add', customerController.addCustomer);
+router.post('/add', customerController.postCustomer);
 
 module.exports = router;
